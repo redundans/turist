be436ac8e273ba734a95f00969b68cd6d9714d21
redundans <jesper@klandestino.se>
2021-04-28 14:51:10 +0200

    New site.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,8 @@
 source 'https://rubygems.org'
 gem 'jekyll'
+gem "github-pages", group: :jekyll_plugins
+
+group :jekyll_plugins do
+    gem "jekyll-paginate"
+  	gem "jekyll-feed", "~> 0.12"
+end
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   purge: [`_site/**/*.html`],
   theme: {
-    extend: {},
+  	screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '768px',
+      'xl': '768px',
+      '2xl': '768px',
+    },
+    extend: {
+      fontFamily: {
+        'display': [ 'Hyperlegible' ]
+      },
+    	colors: {
+    		'base' : '#F6F6F6',
+        'black' : '#092335',
+    	}
+    },
   },
   variants: {},
   plugins: [],
